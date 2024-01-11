e19ec3528aa102b6bb77de74ae34b8738d8e0a5c
DarkoProg <marko.golob99@gmail.com>
2024-01-12 00:46:01 +0100

    press q to quit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    execute, terminal,
+    terminal::{disable_raw_mode, enable_raw_mode},
+    ExecutableCommand,
+};
+use std::error::Error;
+use std::io::{self, stdout, Read};
+
+fn main() -> io::Result<()> {
+    enable_raw_mode()?;
+
+    loop {
+        if let crossterm::event::Event::Key(key_event) = crossterm::event::read()? {
+            if key_event.code == crossterm::event::KeyCode::Char('q') {
+                if key_event.code == crossterm::event::KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    disable_raw_mode()?;
+
+    io::stdout().execute(terminal::Clear(terminal::ClearType::All))?;
+
+    Ok(())
 }
