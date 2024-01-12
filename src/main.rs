306c1346750cc44d28c086585c10c71b202eecc1
DarkoProg <marko.golob99@gmail.com>
2024-01-12 01:13:15 +0100

    qxits with ctr q now

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 use crossterm::{
+    event::{KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
     execute, terminal,
     terminal::{disable_raw_mode, enable_raw_mode},
     ExecutableCommand,
 };
-use std::error::Error;
 use std::io::{self, stdout, Read};
+use std::time::Duration;
 
 fn main() -> io::Result<()> {
     enable_raw_mode()?;
 
     loop {
-        if let crossterm::event::Event::Key(key_event) = crossterm::event::read()? {
-            if key_event.code == crossterm::event::KeyCode::Char('q') {
-                if key_event.code == crossterm::event::KeyCode::Char('q') {
-                    break;
+        if crossterm::event::poll(Duration::from_millis(10))? {
+            if let crossterm::event::Event::Key(key_event) = crossterm::event::read()? {
+                match key_event {
+                    KeyEvent {
+                        code: KeyCode::Char('q'),
+                        modifiers: KeyModifiers::CONTROL,
+                        kind: _,
+                        state: _,
+                    } => {
+                        break;
+                    }
+                    _ => {
+                        println!("{:?}", key_event.code);
+                    }
                 }
             }
         }
