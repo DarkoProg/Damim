dbe0459588cc46a21a47a90a5319eb195d271a4a
DarkoProg <marko.golob99@gmail.com>
2024-01-14 01:45:59 +0100

    refresh screen

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,11 @@
-use crossterm::{
-    event::{KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
-    execute, terminal,
-    terminal::{disable_raw_mode, enable_raw_mode},
-    ExecutableCommand,
-};
-use std::error::Error;
-use std::fmt;
-use std::io::{self, stdout, Read};
-use std::time::Duration;
+#![warn(clippy::all, clippy::pedantic)]
 
-#[derive(Debug)]
-struct CustomError(String);
+mod editor;
+mod terminal;
 
-// Implement the Error trait for the custom error type
-impl fmt::Display for CustomError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Custom Error: {}", self.0)
-    }
-}
-impl Error for CustomError {}
+use editor::Editor;
+use terminal::Terminal;
 
-// Function to handle errors
-fn die(err: impl Error) {
-    panic!("{}", err);
-    // Additional error handling logic can be added here
-}
-
-fn main() -> io::Result<()> {
-    enable_raw_mode().unwrap();
-
-    loop {
-        if crossterm::event::poll(Duration::from_millis(10))? {
-            if let crossterm::event::Event::Key(key_event) =
-                crossterm::event::read().map_err(die).unwrap()
-            {
-                match key_event {
-                    KeyEvent {
-                        code: KeyCode::Char('q'),
-                        modifiers: KeyModifiers::CONTROL,
-                        kind: _,
-                        state: _,
-                    } => {
-                        break;
-                    }
-                    _ => {
-                        println!("{:?}", key_event.code);
-                    }
-                }
-            }
-        }
-    }
-
-    disable_raw_mode().unwrap();
-
-    io::stdout().execute(terminal::Clear(terminal::ClearType::All))?;
-
-    Ok(())
+fn main() {
+    Editor::default().run();
 }
